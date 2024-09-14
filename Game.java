f86b3041b43800e3a8ce7b81b31bbed82835e6ae
Andrew Jonhardt <70071943+AndrewJDGalga@users.noreply.github.com>
2024-09-14 17:00:59 -0500

    trashtable for game.

--- Game.java
+++ Game.java
@@ -2,6 +2,13 @@
 
 public class Game {
     public static void main(String[] args) {
+        String[] trashTable = {
+            "drink",
+            "snack",
+            "caffeine_half",
+            "beer_half",
+            "change_half"
+        };
         
     }
 }
