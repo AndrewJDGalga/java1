3141f5adcbf746ba2367f5f3612273531cee17d6
Andrew Jonhardt <70071943+AndrewJDGalga@users.noreply.github.com>
2024-09-14 17:43:04 -0500

    player, base stats.

--- Game.java
+++ Game.java
@@ -9,12 +9,25 @@ public class Game {
             "beer_half",
             "change_half"
         };
-        for(int i = 0; i < 1000; i++) {
-            if(rollD6() > 6 || rollD6() < 1) System.out.println(rollD6());
-        }
+
+        
     }
 
     static int rollD6() {
         return (int)(Math.random() * 6) + 1;
     }
+}
+
+class Player {
+    static final int dirtyMax = 6;
+    static final int poopMax = 6;
+    static final int peeMax = 4;
+    static final int quenchedMax = 6;
+    static final int satedMax = 6;
+    int dirty = 0;
+    int poop = 0;
+    int pee = 0;
+    int sated = satedMax;
+    int quenched = quenchedMax;
+    
 }
