3a7d93947b8edfa437ff4c8f8302f5304b5e345e
Andrew Jonhardt <70071943+AndrewJDGalga@users.noreply.github.com>
2024-09-16 18:58:30 -0500

    not very good testing, but already realizing my logic is a bit odd. I'm gonna keep this in for now since this is not a good time to learn a new testing framework.

--- Test.java
+++ Test.java
@@ -1,5 +1,19 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Two mains in 1 file");
+        playerStatusTests();
+    }
+    static void playerStatusTests() {
+        int noHunger = 0;
+        int someHunger = 1;
+        int veryHunger = 3;
+        
+        int veryThirst = 3;
+        int someThirst = 1;
+        int noThirst = 6;
+
+        Player pTest = new Player();
+
+        pTest.quenched = noThirst;
+        System.out.println("Feeling moist = " + pTest.getQuenchedStatus());
     }
 }
