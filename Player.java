b4b6520b2bc317410f6111f71bdba475ed4f541b
Andrew Jonhardt <70071943+AndrewJDGalga@users.noreply.github.com>
2024-09-16 18:43:10 -0500

    moved player logic to separate file.

---
+++ Player.java
@@ -0,0 +1,28 @@
+class Player {
+    static final int dirtyMax = 6;
+    static final int poopMax = 6;
+    static final int peeMax = 4;
+    static final int quenchedMax = 6;
+    static final int satiatedMax = 6;
+    int dirty = 0;
+    int poop = 0;
+    int pee = 0;
+    int satiated = satiatedMax;
+    int quenched = quenchedMax;
+    float cash = 0f;
+
+    public String getQuenchedStatus() {
+        String status = "";
+            if(quenched >= quenchedMax - 1) status = "Feeling moist.";
+            else if (quenched >= quenchedMax - 3) status = "Feeling a little thirsty.";
+            else status = "Could really use a drink.";
+        return status;
+    }
+    public String getSatiatedStatus() {
+        String status = "";
+            if(satiated >= satiatedMax - 1) status = "Feeling full.";
+            else if (satiated >= satiatedMax - 3) status = "Feeling a little hungry.";
+            else status = "Could really use a bite.";
+        return status;
+    }
+}
