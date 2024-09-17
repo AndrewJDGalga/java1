834265663cbc3ca22270f8c0aa6e6c3bbadcac86
Andrew Jonhardt <70071943+AndrewJDGalga@users.noreply.github.com>
2024-09-17 18:33:29 -0500

    hunger in same expected range as thirst.

--- Test.java
+++ Test.java
@@ -1,16 +1,27 @@
 public class Test {
     public static void main(String[] args) {
-        playerStatusTests();
+        playerThirstTest();
+        playerSatiationTest();
     }
-    static void playerStatusTests() {
-        int noHunger = 0;
-        int someHunger = 1;
-        int veryHunger = 3;
-        
+    static void playerSatiationTest() {
+        int noHunger = 6;
+        int someHunger = 4;
+        int veryHunger = 2;
+        Player pTest = new Player();
+
+        pTest.satiated = noHunger;
+        System.out.println("Feeling full = " + pTest.getSatiatedStatus());
+
+        pTest.satiated = someHunger;
+        System.out.println("Feeling a little hungry = " + pTest.getSatiatedStatus());
+
+        pTest.satiated = veryHunger;
+        System.out.println("Could really use a bite = " + pTest.getSatiatedStatus());
+    }
+    static void playerThirstTest() {
         int veryThirst = 2;
         int someThirst = 4;
         int noThirst = 6;
-
         Player pTest = new Player();
 
         pTest.quenched = noThirst;
