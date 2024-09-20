5e3c6fbaa1991d0c7e644006fba42bd7c05027ff
Andrew Jonhardt <70071943+AndrewJDGalga@users.noreply.github.com>
2024-09-19 21:32:15 -0500

    max 8 for random patrons with min 1 necessitates additional die-rolling class, if sticking to same structure. Extra method is probably excessive but why not.

--- Location.java
+++ Location.java
@@ -1,4 +1,8 @@
 class Location {
+    static final int minPatrons = 1;
+    static final int maxPatrons = 8;
+    int curPatrons;
+    
     String locationName;
     Location(String name){
         locationName = name;
--- SystemSupport.java
+++ SystemSupport.java
@@ -2,6 +2,12 @@ final class SystemSupport {
     private SystemSupport() {}
     
     static int rollD6() {
-        return (int)(Math.random() * 6) + 1;
+        return rollDie(6);
+    }
+    static int rollD8() {
+        return rollDie(8);
+    }
+    private static int rollDie(int max) {
+        return (int)(Math.random() * max) + 1;
     }
 }
