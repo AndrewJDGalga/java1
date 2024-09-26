caf0a8588d4568fbd6a7ed11fa526a331ede4213
Andrew Jonhardt <70071943+AndrewJDGalga@users.noreply.github.com>
2024-09-26 06:08:19 -0500

    corrected bowels + bladder in Player, using pee + poop was too immature for me.

--- Player.java
+++ Player.java
@@ -2,8 +2,8 @@ import java.util.Arrays;
 
 class Player {
     static final int dirtyMax = 6;
-    static final int poopMax = 6;
-    static final int peeMax = 4;
+    static final int bowelsMax = 6;
+    static final int bladderMax = 4;
     static final int quenchedMax = 6;
     static final int satiatedMax = 6;
     static final int minDice = 2;
@@ -11,8 +11,8 @@ class Player {
     int[] currentDice = new int[maxDice];
     private int currentDiceCount = 3; 
     int dirty = 0;
-    int poop = 0;
-    int pee = 0;
+    int bowels = 0;
+    int bladder = 0;
     int satiated = satiatedMax;
     int quenched = quenchedMax;
     float cash = 0f;
@@ -44,18 +44,28 @@ class Player {
             currentDice[i] = SystemSupport.rollD6();
         }
     }
+    public String getBowelsStatus() {
+        String status = "Peeking!";
+        if(bowels >= bowelsMax - 1) status = "Nice and empty.";
+        else if (bowels >= bowelsMax - 3) status = "Feeling disquieting rumblings.";
+        return status;
+    }
+    public String getBladderStatus() {
+        String status = "Near to bursting!";
+        if(bladder >= bladderMax - 1) status = "Empty.";
+        else if (bladder >= bladderMax - 3) status = "Could release.";
+        return status;
+    }
     public String getQuenchedStatus() {
-        String status = "";
-            if(quenched >= quenchedMax - 1) status = "Feeling moist.";
-            else if (quenched >= quenchedMax - 3) status = "Feeling a little thirsty.";
-            else status = "Could really use a drink.";
+        String status = "Could really use a drink.";
+        if(quenched >= quenchedMax - 1) status = "Feeling moist.";
+        else if (quenched >= quenchedMax - 3) status = "Feeling a little thirsty.";
         return status;
     }
     public String getSatiatedStatus() {
-        String status = "";
-            if(satiated >= satiatedMax - 1) status = "Feeling full.";
-            else if (satiated >= satiatedMax - 3) status = "Feeling a little hungry.";
-            else status = "Could really use a bite.";
+        String status = "Could really use a bite.";
+        if(satiated >= satiatedMax - 1) status = "Feeling full.";
+        else if (satiated >= satiatedMax - 3) status = "Feeling a little hungry.";
         return status;
     }
 }
